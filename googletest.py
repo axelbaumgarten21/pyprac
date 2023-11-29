d1ccd56dfec2181f0eb0c9d9dcfeaeddcea5f0fa
axelbaumgarten21 <85459982+axelbaumgarten21@users.noreply.github.com>
2023-11-29 14:22:34 -0600

    updates

--- googletest.py
+++ googletest.py
@@ -6,21 +6,28 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 import time
 import json
+import os
 
 chrome_driver_path = 'C:\\Program Files (x86)\\Google\\chromedriver.exe'
 url = 'https://www.google.com'
 
-# Create a new instance of the Chrome driver
+
 driver = webdriver.Chrome(executable_path=chrome_driver_path)
 
-# Open the specified URL in the browser
+
 driver.get(url)
 
 element = driver.find_element(By.NAME, "q")
 element.send_keys('wolverine')
 element.submit()
 time.sleep(5)
-capture_path = 'C:/Users/Ax/Pictures/py/wolverine.png'
+
+home_directory = os.path.expanduser("~")
+
+relative_path = os.path.join("Pictures", "py", "wolverine.png")
+
+capture_path = os.path.join(home_directory, relative_path)
+
 driver.save_screenshot(capture_path)
 
 element = driver.find_element(By.NAME, "q")
@@ -28,7 +35,12 @@ element.clear()
 element.send_keys('Jupiter')
 element.submit()
 time.sleep(5)
-capture_path = 'C:/Users/Ax/Pictures/py/Jupiter.png'
+home_directory = os.path.expanduser("~")
+
+relative_path = os.path.join("Pictures", "py", "Jupiter.png")
+
+capture_path = os.path.join(home_directory, relative_path)
+
 driver.save_screenshot(capture_path)
 
 driver.quit()
